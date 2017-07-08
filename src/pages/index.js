fcc05463c4d2d6cef60d2fb96fae275c18723fd3
Jason Weingardt <jweingardt12@gmail.com>
2017-07-08 10:46:42 -0400

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default class Index extends React.Component {
       <div>
         <h1>Hi people</h1>
         <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
+        <p>Now go build something cool.</p>
         <Link to="/page-2/">Go to page 2</Link>
       </div>
     )
